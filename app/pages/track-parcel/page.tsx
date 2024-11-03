d3fffdbe63e03b9f88419896289669cce157a489
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-11-03 13:14:14 +0600

    ok

--- app/pages/track-parcel/page.tsx
+++ app/pages/track-parcel/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import Search from "../../components/Search";
+import {
+  FaBoxOpen,
+  FaBusinessTime,
+  FaCheckDouble,
+  FaTruck,
+} from "react-icons/fa";
 
 export default function page() {
   return (
@@ -12,11 +18,41 @@ export default function page() {
         <Search></Search>
       </div>
       <div className="md:px-6 text-white">
-        <p className="">E63434885</p>
-        <p className="text-lg my-4 font-semibold">Apple Mac-book pro 128 GB RC</p>
-        <div className="md:flex justify-between">
-          <p>11 Jan 2024</p>
-          <p>Estimate Time of Delivery: 27 Jan 2024</p>
+        <div className="border-b-2 border-white pb-4">
+          <p className="">E63434885</p>
+          <p className="text-lg my-4 font-semibold">
+            Apple Mac-book pro 128 GB RC
+          </p>
+          <div className="md:flex justify-between">
+            <p>11 Jan 2024</p>
+            <p>Estimate Time of Delivery: 27 Jan 2024</p>
+          </div>
+        </div>
+        <div className="flex justify-evenly gap-1 py-6">
+          <div className="flex flex-col items-center">
+            <div className="w-7 h-7 flex justify-center items-center bg-white rounded-full">
+              <FaBusinessTime className="text-black"></FaBusinessTime>
+            </div>
+            <p>Processing</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="w-7 h-7 flex justify-center items-center bg-white rounded-full">
+              <FaBoxOpen className="text-black"></FaBoxOpen>
+            </div>
+            <p>Packaging</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="w-7 h-7 flex justify-center items-center bg-white rounded-full">
+              <FaTruck className="text-black"></FaTruck>
+            </div>
+            <p>Shifting</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="w-7 h-7 flex justify-center items-center bg-white rounded-full">
+              <FaCheckDouble className="text-black"></FaCheckDouble>
+            </div>
+            <p>Delivered</p>
+          </div>
         </div>
       </div>
     </div>
